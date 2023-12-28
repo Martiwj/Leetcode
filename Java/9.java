308d3b6b6aef09c8a778f7e8cc7486e5024f441b
Martin wichstrøm jørgensen <mwj@Martin-MacBook-Pro.local>
2023-12-28 21:51:41 +0100

    Valid palindrome

---
+++ Java/9.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String num = x +""; 
+
+        int right = num.length()-1; 
+        int left = 0; 
+
+        while (left < right){
+
+            if(num.charAt(left) != num.charAt(right)){
+                return false; 
+            }
+            right --; 
+            left ++; 
+        }
+
+        return true;  
+    }
+}
