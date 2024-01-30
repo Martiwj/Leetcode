c0f3a7fcd2ee789f533ef0f78297f31393390542
Martin wichstrøm jørgensen <mwj@Martin-MacBook-Pro.local>
2024-01-30 11:22:47 +0100

    Linked List Cycle

---
+++ Java/141.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+  
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        HashSet<ListNode> visited = new HashSet<>(); 
+        
+        ListNode curr = head; 
+
+        while(curr != null){
+            if(visited.contains(curr)){
+                return true; 
+            }
+          
+            visited.add(curr);
+            curr = curr.next; 
+        }
+        return false; 
+        
+    }
+}
