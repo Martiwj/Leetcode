597931c97f2dcfcd29b9ec4a62dd06c353754bc4
Martiwj <Martin.wich.jorg@gmail.com>
2024-04-17 18:34:13 +0200

    Lenght of last word

---
+++ Python/58.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.split()
+        last_word_length = len(s[len(s)-1])
+        return last_word_length
