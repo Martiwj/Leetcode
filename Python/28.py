59708db7ab6db3750891308f89123f4a6248a93c
Martiwj <Martin.wich.jorg@gmail.com>
2024-04-17 18:48:14 +0200

    Find the Index of the First Occurrence in a String

---
+++ Python/28.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """    
+        for part in range(0, len(haystack)):
+            if haystack[part:part+len(needle)] == needle:
+                return part
+            
+        return -1
