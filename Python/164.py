6711d420584dc8326ad83165da6b02ec178625ba
Martiwj <Martin.wich.jorg@gmail.com>
2024-12-04 13:22:10 +0100

    Maximum Gap

---
+++ Python/164.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+    
+        if len(nums) == 1:
+            return 0
+        
+        nums.sort()
+        max_dist = 0
+        for i in range(1,len(nums)):
+            if (diff := nums[i] - nums[i-1]) > max_dist:
+                max_dist = diff 
+            
+    
+        return max_dist
+
+
