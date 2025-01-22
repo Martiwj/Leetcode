6bd14370c97af184512a37b4c573c15d82a7b160
Martin Jørgensen <127878026+Martiwj@users.noreply.github.com>
2025-01-22 15:43:00 +0100

    Delete 125.kt

--- 125.kt
+++
@@ -1,6 +0,0 @@
-class Solution {
-    fun isPalindrome(s: String): Boolean {
-        val normalizedS = s.filter { it.isLetterOrDigit() }.lowercase()
-        return normalizedS == normalizedS.reversed()
-    }
-}
